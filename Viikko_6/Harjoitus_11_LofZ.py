96acc2396e08838b88c43218379f3a78d9449f15
PekkaSun <sunila@sunicom.fi>
2014-11-05 00:31:38 +0200

    Haamutaulukko

--- Viikko_6/Harjoitus_11_LofZ.py
+++ Viikko_6/Harjoitus_11_LofZ.py
@@ -13,6 +13,8 @@ RUUDUN_KOKO = 20
 KENTAN_LEVEYS = (int)(LEVEYS/RUUDUN_KOKO)
 KENTAN_KORKEUS = (int)(KORKEUS/RUUDUN_KOKO)
 
+HAAMUJA = 10 # Haamujen lukumäärä
+
 
 # Pygamen initialisointi
 pygame.init()
@@ -60,12 +62,16 @@ def PiirraHaamu(suunta, x, y):
     if suunta == YLOS:
         ikkuna.blit(haamu_y, (x*RUUDUN_KOKO, y*RUUDUN_KOKO))        
 
-def HaamunAloituspaikka():
-    #Haetaan haamun aloituspaikka
+def HaamujenAloituspaikat():
+    #Haetaan haamujen aloituspaikat
+    k = 0
     for i in range(0,KENTAN_LEVEYS):
         for j in range (0,KENTAN_KORKEUS):
             if Alusta[j][i] == "H":
-                return i, j
+                haamu_paikka_x[k] = i
+                haamu_paikka_y[k] = j
+                k += 1
+                
 # ----------------------------------------------------------------------
 
 def HaeKentta(nimi):
@@ -122,13 +128,22 @@ valmis = False
 askel = 0
 
 # Haamun alkuasetukset ------------------------------------------------------------
-haamu_paikka_x = 0
-haamu_paikka_y = 0
-haamu_paikka_x, haamu_paikka_y = HaamunAloituspaikka()
-haamu_askeleet = 0
-haamu_siirto = 0
-haamu_askeleet_ennen_muutosta = random.randrange(5,21)
-haamu_suunta = random.randrange(1,5) # Valitaan suunta sattumanvaraisesti 1-4
+haamu_paikka_x = []
+haamu_paikka_y = []
+haamu_askeleet = []
+haamu_siirto = []
+haamu_askeleet_ennen_muutosta = []
+haamu_suunta = []
+
+for i in range(0, HAAMUJA):
+    haamu_paikka_x.append(0)
+    haamu_paikka_y.append(0)
+    haamu_askeleet.append(0)
+    haamu_siirto.append(0)
+    haamu_askeleet_ennen_muutosta.append(random.randrange(15,50))
+    haamu_suunta.append(random.randrange(1,5)) # Valitaan suunta sattumanvaraisesti 1-4
+HaamujenAloituspaikat()
+
 # ---------------------------------------------------------------------------------
 
 
@@ -171,45 +186,10 @@ while not valmis:
                     ikkuna.blit(ruoho, (i*RUUDUN_KOKO, j*RUUDUN_KOKO))
     # Piirretään hahmo
     PiirraHahmo(suunta, hahmo_paikka_x, hahmo_paikka_y)
-# -------------------------------------------------------------------------------------------------------------
-    # Siiretään haamua
-    haamu_siirto += 1 # Päivitetään haamu paikkaa vain kerran neljästä näytön päivityskerrasta
-    if haamu_siirto > 4:
-        haamu_siirto = 0
-        haamu_askeleet += 1
-        # Tarkistetaan askeleet ennen muutosta
-        if haamu_askeleet > haamu_askeleet_ennen_muutosta:
-            haamu_askeleet_ennen_muutosta = random.randrange(5,21)
-            haamu_suunta = random.randrange(1,5)
-            
-        if haamu_suunta == VASEN:
-            if haamu_paikka_x > 1  and Alusta[haamu_paikka_y][haamu_paikka_x-1]!="X":
-                haamu_paikka_x -= 1
-            elif Alusta[haamu_paikka_y][haamu_paikka_x-1]=="X":
-                haamu_suunta = OIKEA
-        if haamu_suunta == OIKEA:
-            if haamu_paikka_x <= KENTAN_LEVEYS-2 and Alusta[haamu_paikka_y][haamu_paikka_x+1]!="X":
-                haamu_paikka_x += 1
-            elif Alusta[haamu_paikka_y][haamu_paikka_x+1]=="X":
-                haamu_suunta = VASEN
-        if haamu_suunta == YLOS:
-            if haamu_paikka_y > 1 and Alusta[haamu_paikka_y-1][haamu_paikka_x]!="X":
-                haamu_paikka_y -= 1
-            elif Alusta[haamu_paikka_y-1][haamu_paikka_x]=="X":
-                haamu_suunta = ALAS            
-        if haamu_suunta == ALAS:
-            if haamu_paikka_y <= KENTAN_KORKEUS-2 and Alusta[haamu_paikka_y+1][haamu_paikka_x]!="X":
-                haamu_paikka_y += 1
-            elif Alusta[haamu_paikka_y+1][haamu_paikka_x]=="X":
-                haamu_suunta = YLOS            
-
-    # Piirretään haamu
-    PiirraHaamu(haamu_suunta, haamu_paikka_x, haamu_paikka_y)
-
-    if haamu_paikka_x == hahmo_paikka_x and haamu_paikka_y == hahmo_paikka_y:
+    if Alusta[hahmo_paikka_y][hahmo_paikka_x] == "V":
         while not valmis:
-            # Huonosti kävi -- haamu sai sinut
-            teksti = "Haamu nappasi sinut!"
+            # Huonosti kävi -- hukuit!
+            teksti = "Hukuit!"
             loppu_teksti = fontti.render(teksti, True, valkoinen)
             ikkuna.blit(loppu_teksti,(100, 150))
             pygame.display.flip()
@@ -217,7 +197,55 @@ while not valmis:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_x:
                     # Odotellaan X-näppäintä
-                        valmis = True      
+                        valmis = True    
+# -------------------------------------------------------------------------------------------------------------
+    # Siiretään haamut
+    for h in range(0, HAAMUJA):
+        haamu_siirto[h] += 1 # Päivitetään haamu paikkaa vain kerran neljästä näytön päivityskerrasta
+        if haamu_siirto[h] > 4:
+            haamu_siirto[h] = 0
+            haamu_askeleet[h] += 1
+            # Tarkistetaan askeleet ennen muutosta
+            if haamu_askeleet[h] > haamu_askeleet_ennen_muutosta[h]:
+                haamu_askeleet_ennen_muutosta[h] = random.randrange(15,50)
+                haamu_suunta[h] = random.randrange(1,5)
+            
+            if haamu_suunta[h] == VASEN:
+                if haamu_paikka_x[h] > 1  and Alusta[haamu_paikka_y[h]][haamu_paikka_x[h]-1]!="X":
+                    haamu_paikka_x[h] -= 1
+                elif Alusta[haamu_paikka_y[h]][haamu_paikka_x[h]-1]=="X":
+                    haamu_suunta[h] = OIKEA
+            if haamu_suunta[h] == OIKEA:
+                if haamu_paikka_x[h] <= KENTAN_LEVEYS-2 and Alusta[haamu_paikka_y[h]][haamu_paikka_x[h]+1]!="X":
+                    haamu_paikka_x[h] += 1
+                elif Alusta[haamu_paikka_y[h]][haamu_paikka_x[h]+1]=="X":
+                    haamu_suunta[h] = VASEN
+            if haamu_suunta[h] == YLOS:
+                if haamu_paikka_y[h] > 1 and Alusta[haamu_paikka_y[h]-1][haamu_paikka_x[h]]!="X":
+                    haamu_paikka_y[h] -= 1
+                elif Alusta[haamu_paikka_y[h]-1][haamu_paikka_x[h]]=="X":
+                    haamu_suunta[h] = ALAS            
+            if haamu_suunta[h] == ALAS:
+                if haamu_paikka_y[h] <= KENTAN_KORKEUS-2 and Alusta[haamu_paikka_y[h]+1][haamu_paikka_x[h]]!="X":
+                    haamu_paikka_y[h] += 1
+                elif Alusta[haamu_paikka_y[h]+1][haamu_paikka_x[h]]=="X":
+                    haamu_suunta[h] = YLOS            
+
+        # Piirretään haamu
+        PiirraHaamu(haamu_suunta[h], haamu_paikka_x[h], haamu_paikka_y[h])
+
+        if haamu_paikka_x[h] == hahmo_paikka_x and haamu_paikka_y[h] == hahmo_paikka_y:
+            while not valmis:
+                # Huonosti kävi -- haamu sai sinut
+                teksti = "Haamu nappasi sinut!"
+                loppu_teksti = fontti.render(teksti, True, valkoinen)
+                ikkuna.blit(loppu_teksti,(100, 150))
+                pygame.display.flip()
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_x:
+                        # Odotellaan X-näppäintä
+                            valmis = True      
 # --------------------------------------------------------------------------------------------------------------
     # Rajoitetaan päivitys 25 frameen sekunnissa
     kello.tick(25)
